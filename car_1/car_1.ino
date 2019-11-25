ad84bc8e190e36222f06858d095b1d87eb9522cd
ruiqi li <ruiqi.bce@outlook.com>
2019-11-25 09:29:13 -0600

    inital commit

---
+++ car_1/car_1.ino
@@ -0,0 +1,48 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+RF24 radio(9, 10); // CE, CSN
+
+typedef struct {
+  int button;   
+  bool has;
+} PACKAGE;
+
+const byte address[6] = "23470";
+
+void setup() {
+  Serial.begin(115200);
+  radio.begin();
+  radio.openReadingPipe(0,address);
+  radio.setPALevel(RF24_PA_MAX);
+  radio.startListening();
+  pinMode(A7, OUTPUT);
+  pinMode(A6, OUTPUT);
+  pinMode(A4, OUTPUT);
+  pinMode(A5, OUTPUT);
+  pinMode(A0, OUTPUT);
+  pinMode(4, OUTPUT);
+  digitalWrite(A4, 0);
+  digitalWrite(A5, 0);
+}
+
+int flick = 0;
+void loop() {
+  if(radio.available())
+  {
+    digitalWrite(4, flick);
+    flick = ~flick;
+    PACKAGE p;
+    radio.read(&p, sizeof(p));
+    Serial.print("Key");
+    Serial.print(p.button);
+    Serial.println();
+    if(p.button == 11){
+      digitalWrite(A4, 1);
+    }
+    else if(p.button == 8){
+      digitalWrite(A4, 0);
+    }
+  }
+}
